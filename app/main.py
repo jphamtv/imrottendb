8db79d5a5c125294cea469a19afc1938a9567e66
J <jpham26@gmail.com>
2023-09-02 14:36:15 -0700

    Speed up performance with async

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 # main.py
+import asyncio
+import time
 from environs import Env
 
 # Loads environment variables
@@ -12,8 +14,8 @@ from scraper import (
     get_imdb_rating,
     get_rottentomatoes_url,
     get_rottentomatoes_scores,
-    get_letterbxd_url,
-    get_letterbxd_rating,
+    get_letterboxd_url,
+    get_letterboxd_rating,
     get_commonsense_info,
     get_boxofficemojo_url,
     get_box_office_amounts,
@@ -71,6 +73,11 @@ def search(request: Request, title: str = Form(None)):
 @app.get("/details/{tmdb_id}/{media_type}/")
 async def title_details(request: Request, tmdb_id: str, media_type: str):
     """Show selected title details"""
+    
+    # Start timer
+    start_time0 = time.time()
+
+    # Call TBMD API and get title details
     details = get_title_details(tmdb_id, media_type, TMDB_API_KEY)
     title = details["title"]
     year = details["year"]
@@ -78,26 +85,81 @@ async def title_details(request: Request, tmdb_id: str, media_type: str):
     imdb_url = f"https://www.imdb.com/title/{imdb_id}"
     justwatch_url = details["justwatch_url"]
 
-    imdb_rating = get_imdb_rating(imdb_id)
-    rottentomatoes_url = get_rottentomatoes_url(title, year, media_type)
-    rottentomatoes_scores = get_rottentomatoes_scores(rottentomatoes_url)
-    commonsense_info = get_commonsense_info(title, year)
 
-    if justwatch_url:
-        justwatch_page = get_justwatch_page(justwatch_url)
-    else:
-        justwatch_page = None
+    # imdb_rating = asyncio.create_task(get_imdb_rating(imdb_id))
+    # boxofficemojo_url = asyncio.create_task(get_boxofficemojo_url(imdb_id)) if media_type == "Movie" else None
+    # box_office_amounts = asyncio.create_task(get_box_office_amounts(imdb_id)) if media_type == "Movie" else None
+    # commonsense_info = asyncio.create_task(get_commonsense_info(title, year))
+    # justwatch_page = asyncio.create_task(get_justwatch_page(justwatch_url)) if justwatch_url else None
+    # rottentomatoes_url = asyncio.create_task(get_rottentomatoes_url(title, year, media_type))
+    # letterboxd_url = asyncio.create_task(get_letterboxd_url(title, year)) if media_type == "Movie" else None
+
+
+    # imdb_rating = await imdb_rating
+    # boxofficemojo_url = await boxofficemojo_url
+    # box_office_amounts = await box_office_amounts
+    # commonsense_info = await commonsense_info
+    # justwatch_page = await justwatch_page
+    # rottentomatoes_url = await rottentomatoes_url
+    # letterboxd_url = await letterboxd_url
+
+    # rottentomatoes_scores = asyncio.create_task(get_rottentomatoes_scores(rottentomatoes_url))
+    # letterboxd_rating = asyncio.create_task(get_letterboxd_rating(letterboxd_url))  if media_type == "Movie" else None
+
+    # rottentomatoes_scores = await rottentomatoes_scores
+    # letterboxd_rating = await letterboxd_rating
 
     if media_type == "Movie":
-        letterbxd_url = get_letterbxd_url(title, year)
-        letterbxd_rating = get_letterbxd_rating(letterbxd_url)
-        boxofficemojo_url = get_boxofficemojo_url(imdb_id)
-        box_office_amounts = get_box_office_amounts(boxofficemojo_url)
+        imdb_rating = asyncio.create_task(get_imdb_rating(imdb_id))
+        boxofficemojo_url = asyncio.create_task(get_boxofficemojo_url(imdb_id)) 
+        box_office_amounts = asyncio.create_task(get_box_office_amounts(imdb_id)) 
+        commonsense_info = asyncio.create_task(get_commonsense_info(title, year))
+        justwatch_page = asyncio.create_task(get_justwatch_page(justwatch_url))
+        rottentomatoes_url = asyncio.create_task(get_rottentomatoes_url(title, year, media_type))
+        letterboxd_url = asyncio.create_task(get_letterboxd_url(title, year))
+
+        # Wait for the completion of the tasks
+        imdb_rating = await imdb_rating
+        boxofficemojo_url = await boxofficemojo_url
+        box_office_amounts = await box_office_amounts
+        commonsense_info = await commonsense_info
+        justwatch_page = await justwatch_page if justwatch_url else None
+        rottentomatoes_url = await rottentomatoes_url
+        letterboxd_url = await letterboxd_url
+
+        rottentomatoes_scores = asyncio.create_task(get_rottentomatoes_scores(rottentomatoes_url))
+        letterboxd_rating = asyncio.create_task(get_letterboxd_rating(letterboxd_url))
+
+        # Wait for the completion of the tasks
+        rottentomatoes_scores = await rottentomatoes_scores
+        letterboxd_rating = await letterboxd_rating
+
     elif media_type == "TV":
-        letterbxd_url = None
-        letterbxd_rating = None
+        imdb_rating = asyncio.create_task(get_imdb_rating(imdb_id))      
+        commonsense_info = asyncio.create_task(get_commonsense_info(title, year))
+        justwatch_page = asyncio.create_task(get_justwatch_page(justwatch_url)) if justwatch_url else None
+        rottentomatoes_url = asyncio.create_task(get_rottentomatoes_url(title, year, media_type))
+
+        # Wait for the completion of the tasks
+        imdb_rating = await imdb_rating
+        commonsense_info = await commonsense_info
+        justwatch_page = await justwatch_page
+        rottentomatoes_url = await rottentomatoes_url
+
+        rottentomatoes_scores = asyncio.create_task(get_rottentomatoes_scores(rottentomatoes_url))
+        rottentomatoes_scores = await rottentomatoes_scores
+
+        letterboxd_url = None
+        letterboxd_rating = None
         boxofficemojo_url = None
         box_office_amounts = None
+
+    # End timer and return total
+    end_time0 = time.time()
+    execution_time0 = end_time0 - start_time0
+    print(f"Total: {execution_time0}")
+
+
     return templates.TemplateResponse(
         "details.html", {
         "request": request,
@@ -107,8 +169,8 @@ async def title_details(request: Request, tmdb_id: str, media_type: str):
         "media_type": media_type,
         "rottentomatoes_url": rottentomatoes_url,
         "rottentomatoes_scores": rottentomatoes_scores,
-        "letterbxd_url": letterbxd_url,
-        "letterbxd_rating": letterbxd_rating,
+        "letterboxd_url": letterboxd_url,
+        "letterboxd_rating": letterboxd_rating,
         "commonsense_info": commonsense_info,
         "boxofficemojo_url": boxofficemojo_url,
         "box_office_amounts": box_office_amounts,
@@ -116,10 +178,6 @@ async def title_details(request: Request, tmdb_id: str, media_type: str):
     })
 
 
-# start_time = time.time()
-# # Test how long to run
-# end_time = time.time()
-# execution_time = end_time - start_time
-# print()
-# print(f"Total: {execution_time}")
-# print()
+
+
+
--- app/scraper.py
+++ app/scraper.py
@@ -1,30 +1,45 @@
 # scraping.py
+import asyncio
+import httpx
 import requests
+import time
 from bs4 import BeautifulSoup
 
 
 BASE_URLS = {
     "rottentomatoes": "https://www.rottentomatoes.com/search?search=",
-    "letterbxd": "https://letterboxd.com/search/",
+    "letterboxd": "https://letterboxd.com/search/",
     "commonsensemedia": "https://www.commonsensemedia.org/search/",
     "imdb": "https://www.imdb.com/title/",
     "boxofficemojo": "https://www.boxofficemojo.com/title/",
 }
 
 HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36"
+    "User-Agent": (
+        "Mozilla/5.0 (X11; Linux x86_64; rv:12.0) "
+        "Gecko/20100101 Firefox/12.0"
+    ),
+    "Accept-Language": "en-US",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept": "text/html",
+    "Referer": "https://www.google.com"
 }
 
 
-def make_request(url, headers=None):
-    response = requests.get(url, headers=headers)
-    return BeautifulSoup(response.content, "html.parser")
+# async def make_request_imdb(url, headers=None):
+#     response = await requests.get(url, headers=headers)
+#     return BeautifulSoup(response.text, "html.parser")
+
+async def make_request(url, headers=None):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, headers=headers, timeout=30)
+        return BeautifulSoup(response.content, "html.parser")
 
 
-def get_rottentomatoes_url(title, year, media_type):
+async def get_rottentomatoes_url(title, year, media_type):
     year = year[:4]
     search_url = f"{BASE_URLS['rottentomatoes']}{title.replace(' ', '%20')}"
-    soup = make_request(search_url)
+    soup = await make_request(search_url, HEADERS)
     search_result = soup.find(
         "search-page-media-row",
         {"releaseyear": {year}} if media_type == "Movie" else {"startyear": {year}},
@@ -35,12 +50,80 @@ def get_rottentomatoes_url(title, year, media_type):
         return rottentomatoes_url
     else:
         return None
+    
+
+async def get_letterboxd_url(title, year):
+    search_url = f"{BASE_URLS['letterboxd']}{title.replace(' ', '+')}/"
+    soup = await make_request(search_url, HEADERS)
+    search_results = soup.find_all("span", {"class": "film-title-wrapper"})
+    for result in search_results:
+        # Extract the text content
+        year_element = result.find("small", class_="metadata")
+        # If year matches, get the href from the parent <a> tag
+        if year_element and year_element.text.strip() == year:
+            href = result.find("a")["href"]
+            return f"https://letterboxd.com{href}"
+    return None
 
 
-def get_rottentomatoes_scores(rottentomatoes_url):
+async def get_commonsense_info(title, year):
+    search_url = f"{BASE_URLS['commonsensemedia']}{title.replace(' ', '%20')}"
+    soup = await make_request(search_url, HEADERS)
+    search_results = soup.find_all("div", {"class": "site-search-teaser"})
+    for result in search_results:
+        year_element = result.find("div", class_="review-product-summary")
+        # If year matches, get the href from the parent <a> tag
+        if year_element and year_element.text.strip()[-5:-1] == year:
+            href = result.find("a")["href"]
+            rating_age = result.find("span", {"class": "rating__age"}).text.strip()
+            return {
+                "url": f"https://www.commonsensemedia.org{href}",
+                "rating": rating_age,
+            }
+    return None
+
+
+async def get_imdb_rating(imdb_id):
+    url = f"{BASE_URLS['imdb']}{imdb_id}"
+    # soup = await make_request(url, HEADERS)
+    response = requests.get(url, headers=HEADERS)
+    soup = BeautifulSoup(response.content, "html.parser")
+    # Locate the class that contains the IMDb Rating
+    rating = soup.find(
+        "div", {"data-testid": "hero-rating-bar__aggregate-rating__score"}
+    )
+    return rating.text[:-3] if rating else None
+
+
+async def get_boxofficemojo_url(imdb_id):
+    boxofficemojo_url = f"{BASE_URLS['boxofficemojo']}{imdb_id}/"
+    return boxofficemojo_url
+
+
+async def get_box_office_amounts(imdb_id):
+    url = f"{BASE_URLS['boxofficemojo']}{imdb_id}/"
+    soup = await make_request(url, HEADERS)
+    # Locate the span element that contains the Box Office amounts
+    span_elements = soup.find_all("span", class_="a-size-medium a-text-bold")
+    dollar_amounts = [span.get_text(strip=True) for span in span_elements]
+    return dollar_amounts
+
+
+async def get_justwatch_page(justwatch_url):
+    """Get the JustWatch page url for 'US'"""
+    if justwatch_url:
+        soup = await make_request(justwatch_url, HEADERS)
+        try:
+            link = soup.find('div', class_='homepage')
+        except AttributeError:
+            link = None
+        return link.find('a')['href'] if link else None
+
+
+async def get_rottentomatoes_scores(rottentomatoes_url):
     if not rottentomatoes_url:
         return None
-    soup = make_request(rottentomatoes_url)
+    soup = await make_request(rottentomatoes_url, HEADERS)
     # Get the element that contains the Tomatometer and Audience scores
     score_board = soup.find("score-board")
     if not score_board:
@@ -62,25 +145,10 @@ def get_rottentomatoes_scores(rottentomatoes_url):
     }
 
 
-def get_letterbxd_url(title, year):
-    search_url = f"{BASE_URLS['letterbxd']}{title.replace(' ', '+')}/"
-    soup = make_request(search_url)
-    search_results = soup.find_all("span", {"class": "film-title-wrapper"})
-    for result in search_results:
-        # Extract the text content
-        year_element = result.find("small", class_="metadata")
-        # If year matches, get the href from the parent <a> tag
-        if year_element and year_element.text.strip() == year:
-            href = result.find("a")["href"]
-            return f"https://letterboxd.com{href}"
-    return None
-
-
-def get_letterbxd_rating(letterbxd_url):
-    print(letterbxd_url)
-    if not letterbxd_url:
+async def get_letterboxd_rating(letterboxd_url):
+    if not letterboxd_url:
         return None
-    soup = make_request(letterbxd_url)
+    soup = await make_request(letterboxd_url, HEADERS)
     # Locate the class that contains the Tomatometer and Audience scores
     try:
         rating = soup.find("meta", {"name": "twitter:data2"}).get("content")
@@ -89,52 +157,28 @@ def get_letterbxd_rating(letterbxd_url):
     return round(float(rating[0:5]), 1) if rating else None
 
 
-def get_commonsense_info(title, year):
-    search_url = f"{BASE_URLS['commonsensemedia']}{title.replace(' ', '%20')}"
-    soup = make_request(search_url)
-    search_results = soup.find_all("div", {"class": "site-search-teaser"})
-    for result in search_results:
-        year_element = result.find("div", class_="review-product-summary")
-        # If year matches, get the href from the parent <a> tag
-        if year_element and year_element.text.strip()[-5:-1] == year:
-            href = result.find("a")["href"]
-            rating_age = result.find("span", {"class": "rating__age"}).text.strip()
-            return {
-                "url": f"https://www.commonsensemedia.org{href}",
-                "rating": rating_age,
-            }
-    return None
 
+# def get_movie_ratings(imdb_id: str, media_type: str, title: str, year: str, justwatch_url: str):
 
-def get_imdb_rating(imdb_id):
-    url = f"{BASE_URLS['imdb']}{imdb_id}"
-    soup = make_request(url, HEADERS)
-    # Locate the class that contains the IMDb Rating
-    rating = soup.find(
-        "div", {"data-testid": "hero-rating-bar__aggregate-rating__score"}
-    )
-    return rating.text[:-3] if rating else None
+#     # These can run concurrently
+#     imdb_rating = get_imdb_rating(imdb_id)
+#     box_office_amounts = get_box_office_amounts(imdb_id) if media_type == "Movie" else None
+#     commonsense_info = get_commonsense_info(title, year)
+#     justwatch_page = get_justwatch_page(justwatch_url) if justwatch_url else None
 
+#     rottentomatoes_url = get_rottentomatoes_url(title, year, media_type)
+#     rottentomatoes_scores = get_rottentomatoes_scores(rottentomatoes_url)
 
-def get_boxofficemojo_url(imdb_id):
-    boxofficemojo_url = f"{BASE_URLS['boxofficemojo']}{imdb_id}/"
-    return boxofficemojo_url
+#     letterboxd_url = get_letterboxd_url(title, year) if media_type == "Movie" else None
+#     letterboxd_rating = get_letterboxd_rating(letterboxd_url)  if media_type == "Movie" else None
 
-
-def get_box_office_amounts(boxofficemojo_url):
-    soup = make_request(boxofficemojo_url, HEADERS)
-    # Locate the span element that contains the Box Office amounts
-    span_elements = soup.find_all("span", class_="a-size-medium a-text-bold")
-    dollar_amounts = [span.get_text(strip=True) for span in span_elements]
-    return dollar_amounts
-
-
-def get_justwatch_page(justwatch_url):
-    """Get the JustWatch page url for 'US'"""
-    soup = make_request(justwatch_url, HEADERS)
-    try:
-        link = soup.find('div', class_='homepage')
-    except AttributeError:
-        link = None
-    return link.find('a')['href'] if link else None
-
+#     return {
+#     "imdb_rating": imdb_rating,
+#     "rottentomatoes_url": rottentomatoes_url,
+#     "rottentomatoes_scores": rottentomatoes_scores,
+#     "letterboxd_url": letterboxd_url,
+#     "letterboxd_rating": letterboxd_rating,
+#     "commonsense_info": commonsense_info,
+#     "box_office_amounts": box_office_amounts,
+#     "justwatch_page": justwatch_page,   
+# }
